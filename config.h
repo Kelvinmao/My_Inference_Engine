3f6e97d1fc7956c9ae9dab8cf8b49e81105df8e4
root <root@localhost.localdomain>
2025-09-12 02:22:13 +0000

    Finish basic user inputs, echoes and timing util.

--- config.h
+++ config.h
@@ -12,3 +12,12 @@
 #define COLOR_YELLOW   "\x1b[33m"
 #define COLOR_ORANGE   "\x1b[33m"
 #define COLOR_CYAN     "\033[36m"
+
+//========================================
+// Configs
+//========================================
+#define MAX_LINE_WIDTH 80
+
+constexpr int SEQ_LEN = 8192;
+constexpr int PROMPT_BUFFER_SIZE = 32768;
+constexpr int VOCAB_SIZE = 151936;
--- main.cu
+++ main.cu
@@ -24,6 +24,78 @@ print_banner() {
     )" COLOR_RESET);
 }
 
+long
+time_in_ms() {
+    // return time in milliseconds, for benchmarking the model speed
+    struct timespec time;
+    clock_gettime(CLOCK_REALTIME, &time);
+    return time.tv_sec * 1000 + time.tv_nsec / 1000000;
+}
+
+void construct_path(
+    char* out_path,
+    size_t out_size,
+    const char* dir,
+    const char* filename
+) {
+    size_t len = strlen(dir);
+    if (len > 0 && dir[len-1] == '/') {
+        // there is already a slash at the end
+        snprintf(out_path, out_size, "%s%s", dir, filename);
+    } else {
+        // no slash at the end, add one
+        snprintf(out_path, out_size, "%s/%s", dir, filename);
+    }
+}
+
+// ==========================================
+// chat loop
+// ==========================================
+void read_stdin(
+    const char* guide,
+    char* buffer,
+    size_t bufsize
+) {
+    // read a line from stdin, up to but not including \n
+    printf("%s", guide);
+    if (fgets(buffer, bufsize, stdin) != NULL) {
+        size_t len = strlen(buffer);
+        if (len > 0 && buffer[len - 1] == '\n') {
+            // strip newline
+            buffer[len - 1] = '\0';
+        }
+    }
+}
+
+void chat(
+    char* cli_user_prompt,
+    char* system_prompt,
+    int enable_thinking
+) {
+    char user_prompt[PROMPT_BUFFER_SIZE];
+    int user_turn = 1;
+    int pos = 0;
+
+    while (1) {
+        if (pos >= SEQ_LEN) {
+            printf("\n%s(context window full, clearing)%s\n", COLOR_YELLOW, COLOR_RESET);
+            user_turn = 1;
+            pos = 0;
+        }
+
+        if (user_turn) {
+            if (cli_user_prompt != NULL) {
+                if (pos > 0)    break;
+                strcpy(user_prompt, cli_user_prompt);
+            } else {
+                read_stdin("\n>> ", user_prompt, sizeof(user_prompt));
+                // printf("User input: %s\n", user_prompt);
+                if (!user_prompt[0])    break;
+            }
+        }
+    }
+}
+
 struct Args {
     std::string model_dir;
     float temperature = 0.6f;
@@ -79,6 +151,16 @@ int main(int argc, char* argv[]) {
     print_banner();
     Args args = parse_args(argc, argv);
 
+    // default parameters
+    char* model_dir = NULL;
+    float temperature = 0.6f;
+    float top_p = 0.95;
+    int top_k = 20;
+    char* prompt = NULL;
+    unsigned long long rng_seed = 0;
+    char* system_prompt = NULL;
+    int enable_thinking = 0;
+
     std::cout << "\n[Config]\n";
     std::cout << "  model_dir   = " << args.model_dir << std::endl;
     std::cout << "  temperature   = " << args.temperature << std::endl;
@@ -88,6 +170,8 @@ int main(int argc, char* argv[]) {
     std::cout << "  prompt   = " << args.prompt << std::endl;
     std::cout << "  system_prompt   = " << args.system_prompt << std::endl;
     std::cout << "  enable_thinking   = " << args.enable_thinking << std::endl;
+
+    chat(prompt, system_prompt, enable_thinking);
 
     return 0;
 }
