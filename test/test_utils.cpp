02b7b66a81b1e6a9cb15e7c2a333146441b13ccc
root <root@localhost.localdomain>
2025-09-14 17:11:01 +0000

    Add some test cases for utils and tokenizer

--- test/test_utils.cpp
+++ test/test_utils.cpp
@@ -1,6 +1,33 @@
 #include <catch2/catch_test_macros.hpp>
 #include "../utils.h"
 
-TEST_CASE("example test", "[utils]") {
-    REQUIRE(1 + 1 == 2);
+TEST_CASE("Construct model path", "[model_path]") {
+    // void construct_path(
+    //     char* out_path,
+    //     size_t out_size,
+    //     const char* dir,
+    //     const char* filename
+    // );
+    char out_path[1024];
+    int out_size = 1024;
+
+
+    SECTION( "Concatenate filename WITHOUT slash at the end of dir" ) {
+        const char dir[1024] = "/path/to/model";
+        const char filename[1024] = "model.tensor";
+        construct_path(out_path, out_size, dir, filename);
+        char expected[1024] = "/path/to/model/model.tensor";
+        bool result = (strcmp(out_path, expected) == 0);
+        REQUIRE(result == true);
+    }
+
+    SECTION( "Concatenate filename WITH slash at the end of dir " ) {
+        const char dir[1024] = "/path/to/model/";
+        const char filename[1024] = "model.tensor";
+        construct_path(out_path, out_size, dir, filename);
+        char expected[1024] = "/path/to/model/model.tensor";
+        printf("%s", out_path);
+        bool result = (strcmp(out_path, expected) == 0);
+        REQUIRE(result == true);
+    }
 }
