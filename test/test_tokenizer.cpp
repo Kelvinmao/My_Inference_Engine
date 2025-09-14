f7ea13aec2509710b60ded185b9633d6570f8969
Kelvinmao <kelvinmao@bupt.edu.cn>
2025-09-14 14:39:27 -0400

    Add unit test cases for utils and tokenizer (#4)
    * Finish basic user inputs, echoes and timing util.
    
    * Add Catch2 for unit testing
    
    * Seperate the declaration and implementation of tokenizer and utils
    
    * Add folder for unit testing
    
    * Remove utils in the main.cu
    
    * Fixed the symbo missing issue for Catch2
    
    * Add some test cases for utils and tokenizer
    
    ---------
    
    Co-authored-by: root <root@localhost.localdomain>

--- test/test_tokenizer.cpp
+++ test/test_tokenizer.cpp
@@ -1,6 +1,35 @@
+#include <fstream>
+#include <memory>
 #include <catch2/catch_test_macros.hpp>
 #include "../tokenizer.h"
 
-TEST_CASE("example test", "[tokenizer]") {
-    REQUIRE(1 + 1 == 2);
+TEST_CASE("load_single_template loads file test", "[tokenizer]") {
+    // load_single_template(char* buffer, size_t buffer_size, const char* dir_path, const char* filename);
+    const char* test_dir = ".";
+    const char* test_file = "test_template.txt";
+
+    {
+        std::ofstream ofs(test_file);
+        REQUIRE(ofs.is_open());
+        ofs << "This is the char template";
+    }
+
+    char buffer[256];
+    load_single_template(buffer, sizeof(buffer), test_dir, test_file);
+
+    REQUIRE(std::string(buffer) == "This is the char template");
+
+    std::remove(test_file);
+}
+
+TEST_CASE("build_tokenizer constrct a tokenizer test", "[tokenizer]") {
+    // build_tokenizer(Tokenizer* t, const char* dir_path, int enable_thinking)
+    const char* dir_path = "/path/to/model";
+    auto t =  std::make_unique<Tokenizer>();
+    
+    SECTION ("Test tokenizer construction") {
+        int enable_thinking = 0;
+        build_tokenizer(t.get(), dir_path, enable_thinking);
+        REQUIRE(t->vocab_size == VOCAB_SIZE);
+    }
 }
